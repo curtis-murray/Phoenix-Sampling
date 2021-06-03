be1e2808969ffcbe5433d885e9bcf33a8fb530c3
curtis-murray <murrcurt09@gmail.com>
2021-06-03 23:46:56 +0930

    conda update

--- script.sh
+++ script.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #SBATCH -p batch            	                                # partition (this is the queue your job will be added to) 
-#SBATCH -N 75               	                                # number of nodes (no MPI, so we only use a single node)
-#SBATCH -n 1              	                                # number of cores
+#SBATCH -N 2               	                                # number of nodes (no MPI, so we only use a single node)
+#SBATCH -n 2             	                                # number of cores
 #SBATCH --time=01:00:00    	                                # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
-#SBATCH --mem=2GB         	                                # memory required per node (here set to 4 GB)
+#SBATCH --mem=4GB         	                                # memory required per node (here set to 4 GB)
 
 # Notification configuration 
 #SBATCH --mail-type=END					    	# Send a notification email when the job is done (=END)
 #SBATCH --mail-type=FAIL   					# Send a notification email when the job fails (=FAIL)
 #SBATCH --mail-user=curtis.murray@adelaide.edu.au  		# Email to which notifications will be sent
 
+module load Anaconda3/2020.07
+module load R
+
+conda activate gt
+
 mpirun -np 1 ./my_program.sh
+
+conda deactivate
