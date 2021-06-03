8c50f2661df641ec8fb7caa02da84bf5c1e2b7dc
curtis-murray <murrcurt09@gmail.com>
2021-06-04 01:00:37 +0930

    ncpu

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 #SBATCH -p batch            	                                # partition (this is the queue your job will be added to) 
 #SBATCH -N 2               	                                # number of nodes (no MPI, so we only use a single node)
 #SBATCH -n 2             	                                # number of cores
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=01:00:00    	                                # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
 #SBATCH --mem=4GB         	                                # memory required per node (here set to 4 GB)
 
@@ -15,6 +16,6 @@ module load R
 
 conda activate gt
 
-mpirun -np 1 ./my_program.sh
+mpirun -np 2 ./my_program.sh
 
 conda deactivate
