04570af458fe02196c5c37a961cabb2f243639b7
curtis-murray <murrcurt09@gmail.com>
2021-06-18 23:57:29 +0930

    running

--- my_program.sh
+++ my_program.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm data/Samples/*
+rm data/Samples.info/*
+rm data/Vocab/*
+rm data/Tidy_Topics/*
+rm data/Tree_Distance/*
+
 R CMD BATCH R/phoenix_gen_sample_info.R
 
 source n_samp
@@ -9,7 +15,11 @@ do
 python3.9 Python/phoenix_hSBM.py $i &
 done
 
+wait
+
 R CMD BATCH R/phoenix_tidy.R
+
+wait
 
 for i in $(seq 1 $n_samp)
 do
--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -l                                                  # -l needed to overcome CommandNotFonudError in slurm job
+#!/bin/bash -l
 #SBATCH -p batch            	                                # partition (this is the queue your job will be added to) 
 #SBATCH -N 2               	                                # number of nodes (no MPI, so we only use a single node)
 #SBATCH -n 2             	                                # number of cores
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=01:00:00    	                                # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
-#SBATCH --mem=4GB         	                                # memory required per node (here set to 4 GB)
+#SBATCH --mem=16GB         	                                # memory required per node (here set to 4 GB)
 
 # Notification configuration 
 #SBATCH --mail-type=END					    	# Send a notification email when the job is done (=END)
@@ -14,7 +14,7 @@
 module load Anaconda3/2020.07
 module load R
 
-conda activate /hpcfs/users/$USER/myconda/envs/gt
+conda activate gt
 
 mpirun -np 2 ./my_program.sh
 
