66a850e30edfbf84c449627cd986ecfd06585bc1
Curtis Murray <murrcurt09@gmail.com>
2021-06-23 01:11:24 +0930

    fixing conflicts

--- my_program.sh
+++ my_program.sh
@@ -2,56 +2,24 @@
 
 rm log*
 touch log
-
-echo "Deleting old samples" >> log
 wait
-wait
-echo "Deleting old samples: Done" >> log
-wait
-
-echo "Generating sample info" >> log
-wait
-
 R CMD BATCH R/phoenix_gen_sample_info.R
-
 wait
-echo "Generating sample info: Done" >> log
-
-wait
-
 source n_samp
-
 wait
-
-echo "Running hSBM on $n_samp samples" >> log
-wait
-
-for i in $(seq 0 $n_samp)
+t=0
+for i in $(seq 0 $(($n_samp/5)))
 do
-python3 Python/phoenix_hSBM.py $i &
+  for j in $(seq 1 5)
+  do
+    python3.9 Python/phoenix_hSBM.py $(($i*5+$j-1)) &
+  done
+  wait
 done
-
-wait
-echo "Running hSBM on $n_samp samples: Done" >> log
-
-wait
-
-echo "Tidying data" >> log
-
 wait
 R CMD BATCH R/phoenix_tidy.R
-
-wait
-echo "Tidying data: Done" >> log
-
-wait
-
-echo "Getting tree distances" >> log
 wait
 for i in $(seq 1 $n_samp)
 do
-python3 Python/phoenix_tree_dist.py $i &
+python3.9 Python/phoenix_tree_dist.py $i &
 done
-wait
-echo "Getting tree distances: Done" >> log
-wait
