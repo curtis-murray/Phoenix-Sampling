03b90ec94b62faf1499c3a67ef1aa5fc30412f4c
Curtis Murray <murrcurt09@gmail.com>
2021-06-23 00:45:09 +0930

    redoing the ones that didn't run

--- Python/phoenix_hSBM.py
+++ Python/phoenix_hSBM.py
@@ -8,6 +8,7 @@ import numpy as np
 import re
 from itertools import chain
 import sys
+import glob
 
 # The sample id is passed in as an argument
 sample = int(sys.argv[1])
@@ -17,13 +18,16 @@ sample_info = pd.read_csv("data/Samples.info/samples.csv")
 sample_prop = sample_info.query("sample == @sample")['sample_prop'].iloc[0]
 sample_prop = float(sample_prop) #This stops things from breaking? I don't understand why it's neccesary
 
+if len(glob.glob("data/Samples/words_all_*"+str(sample)+".csv")) > 0:
+    print("Already done " + str(sample))
+    quit()
+
 # These folders should exist but create them if they don't
 if not os.path.exists("data"):
     os.system("mkdir data")
 
 if not os.path.exists("data/Samples"):
     os.system("mkdir data/Samples")
-<<<<<<< HEAD
 
 if not os.path.exists("data/Tidy_Topics"):
     os.system("mkdir data/Tidy_Topics")
@@ -31,9 +35,6 @@ if not os.path.exists("data/Tidy_Topics"):
 if not os.path.exists("data/Tree_Distance"):
     os.system("mkdir data/Tree_Distance")
 
-=======
-    
->>>>>>> 5f48ec99b77a9ead0a4511862584a84d06c8c204
 def run_hSBM(texts, titles, sample_prop, itr):
     # Function to run the hSBM given the data, sample props, and sample ID (now called itr)
     model = sbmtm()
@@ -46,35 +47,25 @@ def run_hSBM(texts, titles, sample_prop, itr):
     # model.load_graph(filename = 'graph.xml.gz')
 
     ## fit the model
-    #gt.seed_rng(32) ## seed for graph-tool's random number generator --> same results
-    model.fit()
-<<<<<<< HEAD
-
+    # Loop a few times to make sure we actually get something!
+    for i in range(10):
+        model.fit()
+        topics = model.topics(l=0,n=10)
+        if len(topics) > 1:
+            break
+    print(model.L)
     for level in range(1,model.L+1):
 
         group_results = model.get_groups(l = level)
         p_w_tw = group_results['p_w_tw']
         pd.DataFrame.to_csv(pd.DataFrame(p_w_tw), "".join(["data/Samples/p_w_tw", str(level),"_", str(sample_prop) , "_", str(itr), ".csv"]))
 
-=======
-        
-    for level in range(1,model.L+1):
-            
-        group_results = model.get_groups(l = level)
-        p_w_tw = group_results['p_w_tw']
-        pd.DataFrame.to_csv(pd.DataFrame(p_w_tw), "".join(["data/Samples/p_w_tw", str(level),"_", str(sample_prop) , "_", str(itr), ".csv"]))
-        
->>>>>>> 5f48ec99b77a9ead0a4511862584a84d06c8c204
     pd.DataFrame.to_csv(pd.DataFrame(model.words), "".join(["data/Samples/words_all_", str(sample_prop) , "_", str(itr),  ".csv"]))
 
 data = pd.read_csv("data/clean_posts.csv")
 
 # Change the name - leaving old name in case anything breaks
-<<<<<<< HEAD
 itr = sample
-=======
-itr = sample    
->>>>>>> 5f48ec99b77a9ead0a4511862584a84d06c8c204
 
 # Sample documents from full data
 sample_ind = np.random.permutation(len(data))[range(round(sample_prop*len(data)))]
---
+++ R/plot_tree_dist.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(tidytext)
+
+Vocab <- read_csv("data/clean_posts.csv") %>% 
+  unnest_tokens(word, Content) %>% 
+  group_by(word) %>%
+  summarise() %>% 
+  arrange(word) %>% 
+  mutate(word_ID_full = 1:n())
+
+weighted_d <- list.files("data/Tree_Distance", full.names = TRUE) %>% 
+  as_tibble() %>% 
+  select(path = value) %>% 
+  mutate(data = map(
+    path, ~read_csv(.x, col_names = c("Sample", "d"))
+  )
+  ) %>% 
+  unnest(data) %>% 
+  left_join(read_csv("data/Samples.info/samples_info.csv"), by = "Sample") %>% 
+  select(-path) %>% 
+  arrange(Sample) %>% 
+  select(Sample, Sample_prop, d)
+
+p <- weighted_d %>% 
+  ggplot() + 
+  geom_point(aes(x = Sample_prop, y = d), alpha = 0.3, show.legend = F) + 
+  geom_smooth(aes(x = Sample_prop, y = d), se = F) + 
+  labs(y = "Distance between Topic Structures",
+       x = "Sampling Proportion") + 
+  theme_minimal() + 
+  lims(y = c(0,3), x = c(0,1)) +
+  theme(legend.position = "bottom")
+
+p
+p %>% ggsave(filename = "Figures/Plots/distance.pdf", 
+             device = "pdf", 
+             height = 6, 
+             width=8)
