af17ca4fc834a9ec6e746f42ddd0d5570f82adcd
murrcurt09 <murrcurt09@gmail.com>
2021-06-29 23:11:31 +0930

    pushign

--- Python/phoenix_tree_dist.py
+++ Python/phoenix_tree_dist.py
@@ -20,48 +20,70 @@ print("Tree distance on sample: " + str(sample))
 if len(glob.glob("data/Tree_Distance/sample_"+str(sample)+".csv")) > 0:
     print("Already done " + str(sample))
     quit()
- 
+
 # Data loading
 # Tidy topics
-df = pd.read_csv("data/Tidy_Topics/tidy_topics_str.csv")
 # Vocab
-Vocab = pd.read_csv("data/Vocab/Vocab.csv")[['word_ID_full', 'freq']].set_index('word_ID_full').T.to_dict('list')
-n_words = len(Vocab)
+Vocab_full = pd.read_csv("data/Vocab/sample_0.csv")[['word_ID_full', 'freq']].set_index('word_ID_full').T.to_dict('list')
+Vocab_samp = pd.read_csv("data/Vocab/sample_"+str(sample)+".csv")[['word_ID_full', 'freq']].set_index('word_ID_full').T.to_dict('list')
+
+n_words = len(Vocab_full)
+
+# Set key-values to 0 for keys not in Vocab_samp
+keys_full = list(Vocab_full.keys())
+keys_sample = list(Vocab_samp.keys())
+
+diff_sample = set(keys_full)-set(keys_sample)
+
+for key in diff_sample:
+    Vocab_samp.update({key:[0]})
 
 # Preprocessing data
 # Filter to full data
-full_data = df.query("Sample == 0")[["word_ID_full","topic"]].set_index('word_ID_full').T.to_dict('list')
+full_data = pd.read_csv("data/Tidy_Topics/sample_0.csv")[["word_ID_full","topic"]].set_index('word_ID_full').T.to_dict('list')
 # Get sample data
-sample_data = df.query("Sample == @sample")[["word_ID_full","topic"]].set_index('word_ID_full').T.to_dict('list')
+sample_data = pd.read_csv("data/Tidy_Topics/sample_"+str(sample)+".csv")[["word_ID_full","topic"]].set_index('word_ID_full').T.to_dict('list')
 
 def total_dist(full_data, sample_data):
-    total_d = 0
+    total_d = [0,0,0,0]
     max_depth_full = len(list(full_data.items())[1][1][0].split("-"))
     max_depth_sample = len(list(sample_data.items())[1][1][0].split("-"))
     # Nested through upper triangle of adjacency matrix computing weighted
     # path length on each itteration
     for i in range(1,n_words+1):
         for j in range(i+1,n_words+1):
-            total_d += weighted_diff_path_length(i,j, full_data, sample_data, max_depth_full, max_depth_sample)
+            part_d = weighted_diff_path_length(i,j, full_data, sample_data, max_depth_full, max_depth_sample)
+            total_d = [total_d[x] + part_d[x] for x in range(0,4)]
     return total_d
 
 def weighted_diff_path_length(i,j, full_data, sample_data, max_depth_full, max_depth_sample):
     # Computed the weighted difference in path lenghts
     # weighted by p_word(i) and p_word(j)
+    d = [0,0,0,0]
+    # Find path lengths
     d_full = path_length(i,j, full_data, max_depth_full)
     d_samp = path_length(i,j, sample_data, max_depth_sample)
-    d = abs((d_full - d_samp)*p_word(i)*p_word(j))
+    # Overall prob of words in corpora
+    p_full_i = Vocab_full.get(i)[0]
+    p_full_j = Vocab_full.get(j)[0]
+    # Prop of words in sample corpus
+    p_samp_i = Vocab_samp.get(j)[0]
+    p_samp_j = Vocab_samp.get(j)[0]
+    # Prob of words in merged sub corpus TODO: weight by total number of words?
+    p_ave_i = (p_full_i + p_samp_i)/2
+    p_ave_j = (p_full_j + p_samp_j)/2
+    # Unweighted distance
+    d[0] = abs(d_full-d_samp)
+    # Corpora weighted distance
+    d[1] = d[0]*p_full_i*p_full_j
+    # Corpus weighted distance
+    d[2] = d[0]*p_ave_i*p_ave_j
+    # Full weighted distance
+    d[3] = abs(d_full*p_full_i*p_full_j - d_samp*p_samp_i*p_samp_j)
     return d
-
-def p_word(i):
-    # Returns p(word | full corpus)
-    # Given as the empirical frequency
-    p = Vocab.get(i)[0]
-    return p
 
 def path_length(i,j,data, max_depth):
     # Funciton to compute path lengths between distinct words
-
     topic_i = data.get(i)
     topic_j = data.get(j)
     # If either or both words are not part of the data return the max path length (2*depth)
@@ -85,4 +107,10 @@ d = total_dist(full_data, sample_data)
 
 print("Distance of " + str(d))
 
-pd.DataFrame({"sample": [sample], "distance": [d]}).to_csv("data/Tree_Distance/sample_"+str(sample)+".csv", index = False, header=False)
+pd.DataFrame({"sample": [sample],
+"distance_unweighted": [d[0]],
+"distance_unweighted": [d[0]],
+"distance_all_subs_weighted": [d[1]],
+"distance_both_subs_weighted": [d[2]],
+"distance_sub_weighted": [d[3]]
+}).to_csv("data/Tree_Distance/sample_"+str(sample)+".csv", index = False, header=False)
--- R/phoenix_tidy.R
+++ R/phoenix_tidy.R
@@ -27,105 +27,130 @@ words_all_all_path <-paste("data/Samples/", words_all_all_path, sep = "")
 # 	select(word = `0`)
 
 Vocab <- read_csv("data/clean_posts.csv") %>% 
-	unnest_tokens(word, Content) %>% 
-	group_by(word) %>%
-	summarise(count = n()) %>% 
-	mutate(freq = count/sum(count)) %>% 
-	arrange(word) %>% 
-	mutate(word_ID_full = 1:n())
+  unnest_tokens(word, Content) %>% 
+  group_by(word) %>%
+  summarise(count = n()) %>% 
+  mutate(freq = count/sum(count)) %>% 
+  arrange(word) %>% 
+  mutate(word_ID_full = 1:n())
 
 write_csv(Vocab, "data/Vocab/Vocab.csv")
 
+# Samples info
+tibble(p_w_tw_all_path = p_w_tw_all_path) %>% 
+  mutate(Sample_prop = str_extract(p_w_tw_all_path, "(?<=_)\\d(.\\d{1,})*(?=_)"),
+         Sample = str_extract(p_w_tw_all_path, "(?<=_)\\d{1,}(?=.csv)"),
+         Level = str_extract(p_w_tw_all_path, "(?<=p_w_tw)\\d{1,}"),
+  ) %>% 
+  map_at(c("Sample_prop", "Sample"), as.double) %>% 
+  as_tibble() %>% 
+  group_by(Sample, Sample_prop) %>% 
+  summarise() %>% 
+  mutate(Sample_prop = round(Sample_prop, 5)) %>%
+  write_csv("data/Samples.info/samples_info.csv")
 # Read all p_w_tw files and get adjacency word-topic matrix, join words, construct full word-topic matrix
 
-probs <- tibble(p_w_tw_all_path = p_w_tw_all_path) %>% 
-	mutate(Sample_prop = str_extract(p_w_tw_all_path, "(?<=_)\\d(.\\d{1,})*(?=_)"),
-				 Sample = str_extract(p_w_tw_all_path, "(?<=_)\\d{1,}(?=.csv)"),
-				 Level = str_extract(p_w_tw_all_path, "(?<=p_w_tw)\\d{1,}"),
-	) %>% 
-	map_at(c("Sample_prop", "Sample","Level"), as.double) %>% 
-	as_tibble() %>% 
-	arrange(-Sample) %>% 
-	mutate(
-		mat = map(
-			p_w_tw_all_path, 
-			~read_csv(.x) %>%
-				select(word_ID = X1, everything()) %>%
-				mutate(word_ID = word_ID + 1) %>% 
-				gather("topic", "p", -word_ID) %>% 
-				mutate(topic = as.numeric(topic) + 1) %>% 
-				filter(p > 0)
-		)) %>% 
-	ungroup() %>% 
-	arrange(Sample, Sample_prop, Level) %>% 
-	mutate(Full = ifelse(Sample_prop == 1,1,0)) %>% 
-	group_by(Sample,Full) %>% 
-	nest() %>% 
-	group_by(Full) %>% 
-	mutate(tmp = 1:n()) %>% 
-	ungroup() %>% 
-	mutate(Full = ifelse((Full) & (tmp == 1),1,0)) %>% 
-	mutate(Sample = ifelse(Full, 0, Sample)) %>% 
-	unnest() %>% 
-	select(Sample,Sample_prop, Level, mat) %>% 
-	arrange(Sample, Level)
+samples <-  words_all_all_path %>% 
+  str_extract("(?<=_)\\d{1,}(?=.csv)")
 
+for(sample in samples){
+  probs <- tibble(p_w_tw_all_path = p_w_tw_all_path) %>% 
+    mutate(Sample_prop = str_extract(p_w_tw_all_path, "(?<=_)\\d(.\\d{1,})*(?=_)"),
+           Sample = str_extract(p_w_tw_all_path, "(?<=_)\\d{1,}(?=.csv)"),
+           Level = str_extract(p_w_tw_all_path, "(?<=p_w_tw)\\d{1,}"),
+    ) %>% 
+    filter(Sample == sample) %>%  # TODO Make this a variable
+    map_at(c("Sample_prop", "Sample","Level"), as.double) %>% 
+    as_tibble() %>% 
+    arrange(-Sample) %>% 
+    mutate(
+      mat = map(
+        p_w_tw_all_path, 
+        ~read_csv(.x) %>%
+          select(word_ID = X1, everything()) %>%
+          mutate(word_ID = word_ID + 1) %>% 
+          gather("topic", "p", -word_ID) %>% 
+          mutate(topic = as.numeric(topic) + 1) %>% 
+          filter(p > 0)
+      )) %>% 
+    ungroup() %>% 
+    arrange(Sample, Sample_prop, Level) %>% 
+    mutate(Full = ifelse(Sample_prop == 1,1,0)) %>% 
+    group_by(Sample,Full) %>% 
+    nest() %>% 
+    group_by(Full) %>% 
+    mutate(tmp = 1:n()) %>% 
+    ungroup() %>% 
+    mutate(Full = ifelse((Full) & (tmp == 1),1,0)) %>% 
+    mutate(Sample = ifelse(Full, 0, Sample)) %>% 
+    unnest() %>% 
+    select(Sample,Sample_prop, Level, mat) %>% 
+    arrange(Sample, Level)
+  
+  
+  # probs %>% 
+  #   select(Sample, Sample_prop) %>% 
+  #   group_by(Sample, Sample_prop) %>% 
+  #   summarise() %>% 
+  #   write_csv("data/Samples.info/samples_info.csv")
+  
+  words_all <- tibble(words_all_all_path = words_all_all_path) %>% 
+    mutate(Sample_prop = str_extract(words_all_all_path, "(?<=_)\\d(.\\d{1,})*(?=_)"),
+           Sample = str_extract(words_all_all_path, "(?<=_)\\d{1,}(?=.csv)")) %>% 
+    filter(Sample == sample) %>% 
+    map_at(c("Sample_prop", "Sample"), as.double) %>% 
+    as_tibble() %>% 
+    mutate(
+      words = map(
+        words_all_all_path,
+        ~read_csv(.x) %>% 
+          mutate(word_ID = X1 + 1, word = `0`) %>% 
+          select(word_ID, word)
+      )) %>% 
+    group_by(Sample) %>% 
+    select(-words_all_all_path) %>% 
+    ungroup() %>% 
+    arrange(Sample, Sample_prop) %>% 
+    mutate(Full = ifelse(Sample_prop == 1,1,0)) %>% 
+    group_by(Full) %>% 
+    mutate(tmp = 1:n()) %>% 
+    ungroup() %>% 
+    mutate(Full = ifelse((Full) & (tmp == 1),1,0)) %>% 
+    mutate(Sample = ifelse(Full, 0, Sample)) %>% 
+    select(Sample,Sample_prop, words) %>% 
+    arrange(Sample)
+  
+  # Do we want ALL the vocab in the sampled topic structures? 
+  # Penalty could be added after easily. Lets try that.
+  
+  tidy_topics_full <- probs %>% 
+    left_join(words_all, by = c("Sample", "Sample_prop")) %>% 	
+    dplyr::mutate(tidy_topics = 
+                    map2(mat, words, ~.x %>%
+                           full_join(.y, by = "word_ID") %>%
+                           left_join(Vocab, by = "word")     # TODO: change to full if necessary
+                    )
+    ) %>% 
+    select(Sample, Sample_prop, Level, tidy_topics) %>% 
+    group_by(Sample, Sample_prop) %>% 
+    ungroup()
+  
+  tidy_topics_full %>% 
+    unnest(tidy_topics) %>% 
+    group_by(Sample, word_ID_full) %>% 
+    arrange(Level) %>% 
+    summarise(topic = paste(topic, collapse = "-")) %>% 
+    write_csv(paste("data/Tidy_Topics/sample_",sample,".csv",sep = ""))
+  
+  tidy_topics_full %>% 
+    filter(Level == max(Level)) %>% 
+    unnest(tidy_topics) %>% 
+    select(word_ID_full, freq = p) %>% 
+    arrange(word_ID_full) %>% 
+    write_csv(paste("data/Vocab/sample_",sample,".csv",sep = ""))
+}
 
-probs %>% 
-	select(Sample, Sample_prop) %>% 
-	group_by(Sample, Sample_prop) %>% 
-	summarise() %>% 
-	write_csv("data/Samples.info/samples_info.csv")
-
-
-words_all <- tibble(words_all_all_path = words_all_all_path) %>% 
-	mutate(Sample_prop = str_extract(words_all_all_path, "(?<=_)\\d(.\\d{1,})*(?=_)"),
-				 Sample = str_extract(words_all_all_path, "(?<=_)\\d{1,}(?=.csv)")) %>% 
-	map_at(c("Sample_prop", "Sample"), as.double) %>% 
-	as_tibble() %>% 
-	mutate(
-		words = map(
-			words_all_all_path,
-			~read_csv(.x) %>% 
-				mutate(word_ID = X1 + 1, word = `0`) %>% 
-				select(word_ID, word)
-		)) %>% 
-	group_by(Sample) %>% 
-	select(-words_all_all_path) %>% 
-	ungroup() %>% 
-	arrange(Sample, Sample_prop) %>% 
-	mutate(Full = ifelse(Sample_prop == 1,1,0)) %>% 
-	group_by(Full) %>% 
-	mutate(tmp = 1:n()) %>% 
-	ungroup() %>% 
-	mutate(Full = ifelse((Full) & (tmp == 1),1,0)) %>% 
-	mutate(Sample = ifelse(Full, 0, Sample)) %>% 
-	select(Sample,Sample_prop, words) %>% 
-	arrange(Sample)
-
-# Do we want ALL the vocab in the sampled topic structures? 
-# Penalty could be added after easily. Lets try that.
-
-tidy_topics_full <- probs %>% 
-	left_join(words_all, by = c("Sample", "Sample_prop")) %>% 	
-	dplyr::mutate(tidy_topics = 
-									map2(mat, words, ~.x %>%
-											 	full_join(.y, by = "word_ID") %>%
-											 	left_join(Vocab, by = "word")     # TODO: change to full if necessary
-									)
-	) %>% 
-	select(Sample, Sample_prop, Level, tidy_topics) %>% 
-	group_by(Sample, Sample_prop) %>% 
-	ungroup()
-
-tidy_topics_full %>% 
-	unnest(tidy_topics) %>% 
-	group_by(Sample, word_ID_full) %>% 
-	arrange(Level) %>% 
-	summarise(topic = paste(topic, collapse = "-")) %>% 
-	write_csv("data/Tidy_Topics/tidy_topics_str.csv")
-
-n_samp <- tidy_topics_full %>% pull(Sample) %>% max()
+#n_samp <- tidy_topics_full %>% pull(Sample) %>% max()
 
 #paste("n_samp=",n_samp, sep = "") %>% 
 #	write(file = "Scripts/n_samp")
