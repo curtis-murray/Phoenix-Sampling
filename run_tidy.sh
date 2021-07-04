4795ea5644d3c8a1bf73e14929bd9dc880b71648
curtis-murray <murrcurt09@gmail.com>
2021-07-04 13:36:33 +0930

    ran tree dist

--- run_tidy.sh
+++ run_tidy.sh
@@ -2,7 +2,7 @@
 #SBATCH -p batch                                                # partition (this is the queue your job will be added to)			$
 #SBATCH -N 1                                                    # number of nodes
 #SBATCH -n 4
-#SBATCH --time=0-01:00:00                                       # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
+#SBATCH --time=0-05:00:00                                       # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
 #SBATCH --mem=16GB                                              # memory required per node (here set to 4 GB)
 
 # Notification configuration
