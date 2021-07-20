dec44558680f920ada1baf4e898cadbe514515ab
curtis-murray <murrcurt09@gmail.com>
2021-07-20 21:05:03 +0930

    tidy

--- run_tidy.sh
+++ run_tidy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -p batch                                                # partition (this is the queue your job will be added to)			$
 #SBATCH -N 1                                                    # number of nodes
-#SBATCH -n 4
+#SBATCH -n 8
 #SBATCH --time=0-05:00:00                                       # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
 #SBATCH --mem=16GB                                              # memory required per node (here set to 4 GB)
 
